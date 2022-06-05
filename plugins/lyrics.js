4e5019cf187ac32c3abfb24e7f93e9149f6ef3a2
mohamed bella <90554606+mohamedBella2000@users.noreply.github.com>
2022-06-06 00:47:28 +0100

    Update lyrics.js

--- plugins/lyrics.js
+++ plugins/lyrics.js
@@ -5,6 +5,7 @@ let handler = async (m, { conn, text, usedPrefix, command }) => {
     if (!teks) throw `Use example ${usedPrefix}${command} hallo`
     const result = await lyricsv2(teks).catch(async _ => await lyrics(teks))
     m.reply(`
+ insta ❤️ *@wox_bella*
 🔮𝗟𝗬𝗥𝗜𝗖𝗦 *${result.title}*
 🔮𝗔𝗨𝗧𝗛𝗢𝗥 ${result.author}
 
