e5ade3b5f090ae2a916c2c4744e76c19ab65a77a
mohamed bella <90554606+mohamedBella2000@users.noreply.github.com>
2022-06-06 00:44:44 +0100

    Update owner-kick.js

--- plugins/owner-kick.js
+++ plugins/owner-kick.js
@@ -8,7 +8,7 @@ let handler = async (m, { conn, participants }) => {
             kickedUser.concat(res)
             await delay(1 * 1000)
         }
-    m.reply(`Success kick ${kickedUser.map(v => '@' + v.split('@')[0])}`, null, { mentions: kickedUser })
+    m.reply(`تم طرد بنجاح ${kickedUser.map(v => '@' + v.split('@')[0])}`, null, { mentions: kickedUser })
 
 }
 handler.help = ['kick']
